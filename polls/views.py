508c4850bbb865c9b4ff9240752370c39de570f4
Lior Noy <ironoy@gmail.com>
2021-02-17 16:27:03 +0200

    Fix flake8 errors in all python files

--- polls/views.py
+++ polls/views.py
@@ -11,13 +11,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
@@ -27,7 +24,8 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-    
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
