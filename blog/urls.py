530fa1abfd654c2add806b890637fb07aa58caa9
Lior Noy <ironoy@gmail.com>
2021-02-23 22:02:36 +0200

    Delete app_name = 'blog'

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'blog'
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path('', views.home, name= 'blog-home'),
+    path('about/', views.about, name= 'blog-about'),
    ]
