cea7cc1c07c313d803d4c8bd9685a9ed59f58671
Lior Noy <ironoy@gmail.com>
2021-02-23 14:08:10 +0200

    Add blog config to INSTALLED_APPS
    This addition helps django to find the templates
    and models for database

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.app.BlogConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
